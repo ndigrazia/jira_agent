e6ff5e4c2b65faae6bb79c2dddea4173b412ee0a
Ubuntu <ollama@a-sand-ollama-vm.naw4h3gdcvhe3i4l2fif4e13jd.bx.internal.cloudapp.net>
2026-06-29 17:58:48 +0000

    Uncomment deploy command in lab.sh

--- lab.sh
+++ lab.sh
@@ -19,9 +19,9 @@ gcloud config list --format='text(core.project, compute.region)'
 echo "GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT"
 echo "GOOGLE_CLOUD_LOCATION=$GOOGLE_CLOUD_LOCATION"
 
-#adk deploy cloud_run \
-#  --project=$GOOGLE_CLOUD_PROJECT \
-#  --region=$GOOGLE_CLOUD_LOCATION \
-#  --service_name=jira-agent-service \
-#  --with_ui \
-#  ./jira_agent
+adk deploy cloud_run \
+  --project=$GOOGLE_CLOUD_PROJECT \
+  --region=$GOOGLE_CLOUD_LOCATION \
+  --service_name=jira-agent-service \
+  --with_ui \
+  ./jira_agent
